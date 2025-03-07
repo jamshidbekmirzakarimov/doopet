299337ea968e42a8055b0ba10fa4412e4aed5311
jamshidbekmirzakarimov <jamshidbekmirzakarimov6@gmail.com>
2025-03-07 22:22:41 +0500

    initial commit

---
+++ system/storage/cache/template/94/9427218204be396997664ab39432584c.php
@@ -0,0 +1,71 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* common/footer.twig */
+class __TwigTemplate_a6147047b07262bf9c324893f08fafba extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<footer>
+   <div class=\"container\">
+    <a href=\"https://opencart-3x.ru/\" target=\"_blank\">";
+        // line 3
+        echo ($context["text_project"] ?? null);
+        echo "</a>|<a href=\"https://opencart-3x.ru/oc3-administration/\" target=\"_blank\">";
+        echo ($context["text_documentation"] ?? null);
+        echo "</a>|<a href=\"https://opencart-3x.ru/blog/\" target=\"_blank\">";
+        echo ($context["text_support"] ?? null);
+        echo "</a><br />
+  </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  41 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "common/footer.twig", "");
+    }
+}
