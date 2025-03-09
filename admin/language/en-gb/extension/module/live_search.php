991fbb608ec528e7f6f0bb3e1a629e08714b9fe2
jamshidbekmirzakarimov <jamshidbekmirzakarimov6@gmail.com>
2025-03-09 23:02:05 +0500

    header bottom styel done

---
+++ admin/language/en-gb/extension/module/live_search.php
@@ -0,0 +1,39 @@
+<?php
+
+// Heading Goes here:
+$_['heading_title'] = '<strong style="color:#41637d">[OpencartModules.com] —</strong> Ajax Live Search <a href="https://opencartmodules.com/?utm_source=my-modules&utm_medium=download" target="_blank" title="OpencartModules.com - Площадка с лучшими модулями для Opencart"><img style="margin-left:15px;height:35px;margin-top:10px;margin-bottom:10px;" src="https://opencartmodules.com/image/cache/catalog/logo/opencartmodules-logo-black-700-175-ru-700x175.png" alt="OpencartModules.com - Площадка с лучшими модулями для Opencart"/></a>';
+
+// Text
+$_['text_extension']   		   = 'Extensions';
+$_['text_edit']                = 'Edit Ajax Live Search Module';
+$_['text_success']             = 'Success: You have modified Ajax Live Search module !';
+$_['text_view_all_results']    = 'View all results';
+
+// Entry
+$_['entry_limit']              = 'Search Limit';
+$_['entry_width']              = 'Image Width';
+$_['entry_height']             = 'Image Height';
+$_['entry_title_length']       = 'Title Length';
+$_['entry_description_length'] = 'Description Length';
+$_['entry_show_image']         = 'Show image';
+$_['entry_show_price']         = 'Show price';
+$_['entry_show_description']   = 'Show description';
+$_['entry_show_add_button']    = 'Show addToCart link';
+$_['entry_status']             = 'Status';
+$_['entry_view_all_results']   = 'Text of view all results link';
+$_['entry_min_length']   	   = 'Search Min Length';
+
+// Help
+$_['help_length']              = 'Show \'...\' when length over this.';
+$_['help_view_all_results']    = 'Text link of search results bottom';
+
+// Error
+$_['error_permission']         = 'Warning: You do not have permission to modify Live Search module !';
+$_['error_limit']              = 'Search Limit required!';
+$_['error_width']              = 'Image Width required!';
+$_['error_height']             = 'Image Height required!';
+$_['error_title_length']       = 'Image Height required!';
+$_['error_description_length'] = 'Description Length required!';
+$_['error_view_all_results']   = 'Text of view all results link required!';
+$_['error_min_length']   	   = 'Search Min Length required!';
+?>
